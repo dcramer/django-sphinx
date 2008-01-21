b0a0d571bc14590a56b85a29f9d96a4c36ca2f57
David Cramer <dcramer@gmail.com>
2008-01-21 20:48:17 +0000

    initial commit to svn repository; renamed django-sphinx to djangosphinx so it's a valid python module name

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='djangosphinx',
+      version='1.0',
+      description='Sphinx Full-text Search',
+      author='Curse, Inc.',
+      author_email='dcramer@curse.com',
+      url='http://code.curse.com/django-sphinx/',
+      packages=['djangosphinx'],
+     )
