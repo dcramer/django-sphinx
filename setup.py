20b3daac8d40e2d211247d8aef6cb6c4685d8534
David Cramer <dcramer@gmail.com>
2008-05-12 16:28:47 +0000

    updated package information

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='djangosphinx',
-      version='1.0',
+      version='2.0',
       description='Sphinx Full-text Search',
       author='David Cramer',
-      author_email='dcramer@curse.com',
-      url='http://code.curse.com/django-sphinx/',
+      author_email='dcramer@gmail.com',
+      url='http://code.curse.com/p/django-sphinx/',
       packages=['djangosphinx'],
      )
