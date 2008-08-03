92b6dd7d069e98700c2204a12486191f03112939
David Cramer <dcramer@gmail.com>
2008-08-03 00:08:38 +0000

    changed default back to api107

--- djangosphinx/constants.py
+++ djangosphinx/constants.py
@@ -4,4 +4,4 @@ __all__ = ('SPHINX_API_VERSION',)
 
 # 0x113 = 1.19
 # 0x107 = 1.17
-SPHINX_API_VERSION = getattr(settings, 'SPHINX_API_VERSION', 0x113)
+SPHINX_API_VERSION = getattr(settings, 'SPHINX_API_VERSION', 0x107)
