793dcc61f2e8f61429b465aa23b2c6161976adc9
David Cramer <dcramer@gmail.com>
2008-08-22 20:08:50 +0000

    removed 2nd description

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from distutils.core import setup
 setup(
     name='djangosphinx',
     version='2.0',
-    description='Sphinx Full-text Search',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://code.google.com/p/django-sphinx/',
