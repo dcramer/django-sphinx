6841e74c9e85860c1d91c7be95a5b9f4df3bba6e
David Cramer <dcramer@gmail.com>
2008-08-22 20:02:12 +0000

    fixed distutils

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@
 
 from distutils.core import setup
 
-setup(name='djangosphinx',
-      version='2.0',
-      description='Sphinx Full-text Search',
-      author='David Cramer',
-      author_email='dcramer@gmail.com',
-      url='http://code.google.com/p/django-sphinx/',
-      packages=['djangosphinx'],
-     )
+setup(
+    name='djangosphinx',
+    version='2.0',
+    description='Sphinx Full-text Search',
+    author='David Cramer',
+    author_email='dcramer@gmail.com',
+    url='http://code.google.com/p/django-sphinx/',
+    description = 'An integration layer bringing Django and Sphinx Search together.',
+    packages = ['djangosphinx', 'djangosphinx.apis', 'djangosphinx.apis.api263', 'djangosphinx.apis.api275', 'djangosphinx.management', 'djangosphinx.management.commands', 'djangosphinx.utils'],
+    package_data={'djangosphinx': ['templates/*']},
+)
