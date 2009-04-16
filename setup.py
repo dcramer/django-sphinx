bfc91181c1638fc28f32413886f80b4967a8f3d3
David Cramer <dcramer@gmail.com>
2009-04-16 19:14:34 +0000

    Renamed project

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(
-    name='djangosphinx',
-    version='2.0',
+    name='django-sphinx',
+    version='2.01',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://code.google.com/p/django-sphinx/',
     description = 'An integration layer bringing Django and Sphinx Search together.',
     packages = ['djangosphinx', 'djangosphinx.apis', 'djangosphinx.apis.api263', 'djangosphinx.apis.api275', 'djangosphinx.management', 'djangosphinx.management.commands', 'djangosphinx.utils'],
-    package_data={'djangosphinx': ['templates/*']},
+    package_data={'djangosphinx': ['templates/*', 'apis/api263/templates/*']},
 )
