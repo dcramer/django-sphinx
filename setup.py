cb16fc6c2da6b82b8c1b478135270fed9c290b86
David Cramer <dcramer@gmail.com>
2009-06-19 22:52:11 +0000

    api278 was not included in setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author_email='dcramer@gmail.com',
     url='http://code.google.com/p/django-sphinx/',
     description = 'An integration layer bringing Django and Sphinx Search together.',
-    packages = ['djangosphinx', 'djangosphinx.apis', 'djangosphinx.apis.api263', 'djangosphinx.apis.api275', 'djangosphinx.management', 'djangosphinx.management.commands', 'djangosphinx.utils'],
+    packages = ['djangosphinx', 'djangosphinx.apis', 'djangosphinx.apis.api263', 'djangosphinx.apis.api275', 'djangosphinx.apis.api278', 'djangosphinx.management', 'djangosphinx.management.commands', 'djangosphinx.utils'],
     package_data={'djangosphinx': ['templates/*', 'apis/api263/templates/*']},
 )
