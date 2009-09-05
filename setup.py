063bfef370400a272e1ada608d612ad0c19712f0
David Cramer <dcramer@david-cramers-macbook.local>
2009-09-04 20:19:27 -0500

    Fixes + deprecations for new version stuff

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 
 from setuptools import setup, find_packages
 
-from djangosphinx import version
+import djangosphinx
 
 setup(
     name='django-sphinx',
-    version=".".join(map(str, version.__version__)),
+    version=".".join(map(str, djangosphinx.__version__)),
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/django-sphinx',
