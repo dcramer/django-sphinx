8fa9d8399bfd9dbed10967c3732d5b7fd3847e95
David Cramer <dcramer@david-cramers-macbook.local>
2009-09-04 19:56:26 -0500

    added djangosphinx.__build__

--- djangosphinx/__init__.py
+++ djangosphinx/__init__.py
@@ -30,8 +30,33 @@ default settings.py values
     SPHINX_PORT = 3312
 </code>
 """
+import os.path
 
 __version__ = (2, 0, 3)
+
+def _get_git_revision(path):
+    revision_file = os.path.join(path, 'refs', 'heads', 'master')
+    if not os.path.exists(revision_file):
+        return None
+    fh = open(revision_file, 'r')
+    try:
+        return fh.read()
+    finally:
+        fh.close()
+
+def get_revision():
+    """
+    :returns: Revision number of this branch/checkout, if available. None if
+        no revision number can be determined.
+    """
+    package_dir = os.path.dirname(__file__)
+    checkout_dir = os.path.normpath(os.path.join(package_dir, '..'))
+    path = os.path.join(checkout_dir, '.git')
+    if os.path.exists(path):
+        return _get_git_revision(path)
+    return None
+
+__build__ = get_revision()
 
 from manager import *
 from utils import generate_config_for_model, generate_config_for_models
